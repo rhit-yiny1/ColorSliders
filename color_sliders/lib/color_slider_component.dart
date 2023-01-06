bf05c9a9c3323fee8f3ad7ca103a0efdaf40d573
rhit-yiny1 <89856239+rhit-yiny1@users.noreply.github.com>
2023-01-06 13:17:39 -0500

    finished dev color slider

--- color_sliders/lib/color_slider_component.dart
+++ color_sliders/lib/color_slider_component.dart
@@ -4,12 +4,14 @@ class ColorSlider extends StatelessWidget {
   final String title;
   final double value;
   final MaterialColor color;
+  final Function onChange;
 
   const ColorSlider(
       {super.key,
       required this.title,
       required this.value,
-      required this.color});
+      required this.color,
+      required this.onChange});
 
   @override
   Widget build(BuildContext context) {
@@ -26,18 +28,19 @@ class ColorSlider extends StatelessWidget {
                 style: const TextStyle(fontSize: 20.0),
               ),
               Text(
-                "$value",
+                value.toStringAsFixed(3),
                 style: const TextStyle(fontSize: 20.0),
               ),
             ],
           ),
           Slider(
             value: value,
+            //divisions: 1000,
             thumbColor: color,
             activeColor: color,
             //inactiveColor: Colors.yellow,
             onChanged: (double value) {
-              print("ok");
+              onChange(value);
             },
           )
         ],
--- color_sliders/lib/color_sliders_page.dart
+++ color_sliders/lib/color_sliders_page.dart
@@ -20,13 +20,39 @@ class _MyWidgetState extends State<MyWidget> {
         ),
         body: Column(
           children: [
-            ColorSlider(title: "Red", value: redValue, color: Colors.red),
-            ColorSlider(title: "Green", value: greenValue, color: Colors.green),
-            ColorSlider(title: "Blue", value: blueValue, color: Colors.blue),
+            ColorSlider(
+                title: "Red",
+                value: redValue,
+                color: Colors.red,
+                onChange: (newValue) {
+                  setState(() {
+                    redValue = newValue;
+                  });
+                }),
+            ColorSlider(
+                title: "Green",
+                value: greenValue,
+                color: Colors.green,
+                onChange: (newValue) {
+                  setState(() {
+                    greenValue = newValue;
+                  });
+                }),
+            ColorSlider(
+              title: "Blue",
+              value: blueValue,
+              color: Colors.blue,
+              onChange: (newValue) {
+                setState(() {
+                  blueValue = newValue;
+                });
+              },
+            ),
             Expanded(
               child: Container(
                 margin: EdgeInsets.all(20.0),
-                color: Colors.red,
+                color: Color.fromRGBO((redValue * 255).round(),
+                    (greenValue * 255).round(), (blueValue.round() * 255), 1.0),
               ),
             )
           ],
